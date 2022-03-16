f4ac2d761c14862bd7e3112c88c57edf5e46f2d6
Kara Erickson <4302497+rhifynnauso@users.noreply.github.com>
2022-03-16 16:56:35 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DocsComponent } from './docs/docs.component';
+import { HomeComponent } from './home/home.component';
+import { LearnComponent } from './learn/learn.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'docs', component: DocsComponent},
+  {path: 'learn', component: LearnComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocsComponent } from './docs/docs.component';
+import { LearnComponent } from './learn/learn.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocsComponent,
+    LearnComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
